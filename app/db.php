1629ca2c1c9f8f4236c79d0d2c3ff1b104fbc259
baraka2003 <barakamulimila@gmail.com>
2025-05-11 15:23:37 +0300

    update

--- app/db.php
+++ app/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = getenv("MYSQLHOST") ?: "localhost";
-$user = getenv("MYSQLUSER") ?: "root";
-$pass = getenv("MYSQLPASSWORD") ?: "";
-$db   = getenv("MYSQLDATABASE") ?: "hostel_db";
+$host = getenv("MYSQLHOST");
+$user = getenv("MYSQLUSER");
+$pass = getenv("MYSQLPASSWORD");
+$db   = getenv("MYSQLDATABASE");
 $port = getenv("MYSQLPORT") ?: 3306;
 
 $conn = new mysqli($host, $user, $pass, $db, $port);
